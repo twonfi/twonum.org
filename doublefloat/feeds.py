91f9d55ec1777b9409baab915cd4fc16b771c119
twonfi <git@twonum.org>
2025-07-31 20:54:56 -0700

    RSS feed

---
+++ doublefloat/feeds.py
@@ -0,0 +1,32 @@
+from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Rss201rev2Feed, Atom1Feed
+
+from .models import Post
+from core.templatetags.markdown_extras import sanitized_markdown
+
+
+# noinspection PyMethodMayBeStatic
+class DoubleFloatFeed(Feed):
+    title = "DoubleFloat"
+    link = "/posts/"  # To avoid circular import with reverse()
+    description = "The twonum blog"
+
+    def items(self):
+        return Post.objects.order_by("-date")
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return sanitized_markdown(item.body)
+
+    def item_author_name(self, item):
+        return item.author
+
+
+class DoubleFloatRSSFeed(DoubleFloatFeed):
+    feed_type = Rss201rev2Feed
+
+
+class DoubleFloatAtomFeed(DoubleFloatRSSFeed):
+    feed_type = Atom1Feed
--- doublefloat/urls.py
+++ doublefloat/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from .views import *
+from .feeds import DoubleFloatRSSFeed, DoubleFloatAtomFeed
 
 app_name = "doublefloat"
 
 urlpatterns = [
     path("<slug:slug>/", view_post, name="view_post"),
+    path("feed.rss", DoubleFloatRSSFeed(), name="rss_feed"),
+    path("feed.atom", DoubleFloatAtomFeed(), name="atom_feed"),
     path("", home, name="home"),
 ]
