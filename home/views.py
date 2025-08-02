687d5a5d12f3ece80e9131000768a43ed5c1ca57
twonfi <git@twonum.org>
2025-08-01 22:47:49 -0700

    fix(home.views): Use "latest or none"

--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Model
 
 from projects.models import Project
 from doublefloat.models import Post
+
+
+def _latest_or_none(model: type[Model], *args):
+    try:
+        m = model.objects.latest(*args)
+    except model.DoesNotExist:
+        return None
+    else:
+        return m
 
 
 def home(request):
@@ -9,8 +19,8 @@ def home(request):
         "title": "twonum.org",
         "show_site_name_in_title": False,
         # "h1_from_title": False,
-        "post": Post.objects.latest("date"),
-        "project": Project.objects.latest("id"),
+        "post": _latest_or_none(Post, "date"),
+        "project": _latest_or_none(Project, "id"),
         "user_agent": request.headers.get("User-Agent"),
         "project_count": Project.objects.count(),
     }
