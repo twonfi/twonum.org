7d04b502184a3a9dd483cc05346421477b241180
twonfi <git@twonum.org>
2025-08-02 03:01:50 -0700

    Use correct DoubleFloat capitalization in admin

--- doublefloat/apps.py
+++ doublefloat/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DoublefloatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'doublefloat'
+    verbose_name = 'DoubleFloat'
