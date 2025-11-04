47e07bafc9f86ddf26620013b9aafbf91820e6a9
twonfi <git@twonum.org>
2025-11-03 17:22:01 -0800

    fix(core): Render 500 template file instead of 404

--- core/views.py
+++ core/views.py
@@ -15,7 +15,7 @@ def error_404(request, exception):
 
 # noinspection PyUnusedLocal
 def error_500(request):
-    resp = render(request, "core/errors/404.txt", status=500)
+    resp = render(request, "core/errors/500.txt", status=500)
     resp["Content-Type"] = "text/plain; charset=utf-8"
     resp["The-Truly-Evil-Devs"] = "Status: 500"
     return resp
