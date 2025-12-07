20cf60e357b2d7eae088f2603d69a581d8e07e03
twonfi <git@twonum.org>
2025-12-07 12:08:04 -0800

    Revert "Use `CompressedStaticFilesStorage`"
    Testing hashing static files.
    
    This reverts commit 5c88ed05bc65d040d60ed54ba95be7ae84061b94.

--- twonumorg/settings.py
+++ twonumorg/settings.py
@@ -239,7 +239,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
